6a4fb4cb1d9a087906f798075dac0450e688b88f
sergiofdf <sergiofdf@gmail.com>
2025-04-07 22:43:59 -0300

    feat: add grafana e prometheus

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -63,7 +63,7 @@ builder.Services.AddScoped<IUserRepository, UserRepository>()
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerConfig();
+builder.Services.AddSwaggerConfig(builder.Configuration);
 builder.AddMassTransitConfig();
 
 builder.Services.AddTransient<ExceptionHandlingMiddleware>();
@@ -90,7 +90,7 @@ app.MapHealthChecks("/health", new HealthCheckOptions
 
 app.UseHttpMetrics();
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
